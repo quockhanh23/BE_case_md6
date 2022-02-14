9d5ececb5aca16c9804373b7a11c2d1400121c86
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-02-14 11:42:45 +0700

    them bang moi sprint 2

---
+++ src/main/java/com/example/backendmd6/model/ApplyNow.java
@@ -0,0 +1,24 @@
+package com.example.backendmd6.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ApplyNow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "fileCVId")
+    FileCV fileCVId;
+    @ManyToOne
+    @JoinColumn(name = "recruitment_id")
+    Recruitment recruitmentId;
+    @ManyToOne
+    @JoinColumn(name = "statusConfirm_id")
+    StatusConfirmOfApplyNow statusConfirmId;
+}
---
+++ src/main/java/com/example/backendmd6/model/Offer.java
@@ -0,0 +1,18 @@
+package com.example.backendmd6.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "recruitment_id")
+    Recruitment recruitmentId;
+}
